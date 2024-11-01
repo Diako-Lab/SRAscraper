62e1b8ecb8ca832204c5a318439f17cefac313ed
JakeLehle <jakelehle@gmail.com>
2024-11-01 18:10:48 -0500

    CLI updated in prep vor 0.1.0

---
+++ snakemake_wrapper/scripts/fastq_download.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import os, sys
+
+os.chdir(output_dir)
+
+#%% Import in the dictionary with the metadata
+import pickle
+
+with open('dictionary_file.pkl', 'rb') as pkl_file:
+     gse_dict = pickle.load(pkl_file)
+     print('Dictionary loaded successfully')
+
+     
+#%% Go get some files
+import subprocess
+
+for key in gse_dict.keys():
+    for accession in gse_dict[key]['SRR']:
+        print(f"\nProcessing sample {accession} from the BioProject {key}")
+        subprocess_1 = subprocess.Popen(
+            ["parallel-fastq-dump", "--sra-id", accession, "--threads", computing_threads, "--outdir", 
+             output_dir + '/fastq/'+key+'/'+accession, "--split-spot", "--split-files", "--gzip"], stdout=subprocess.PIPE, text=True)
+        output, error = subprocess_1.communicate()
+        print(f'Outputs: {output}')
+        print(f'Errors: {error}')
+        print(f"\nRenamming {accession} fastqs")
+        try:
+            os.rename(output_dir + '/fastq/' + key + '/' + accession + '/' + accession + '_1.fastq.gz',
+                    output_dir + '/fastq/' + key + '/' + accession + '/' + accession + '_S1_L001_R1_001.fastq.gz')
+            os.rename(output_dir + '/fastq/' + key + '/' + accession + '/' + accession + '_2.fastq.gz',
+                    output_dir + '/fastq/' + key + '/' + accession + '/' + accession + '_S2_L001_R1_001.fastq.gz')
+        except FileNotFoundError:
+            print("File not found.")
+        except OSError as e:
+            print("Error renaming file:", e)
+
+
+#%% End file
+
+sys.exit()
--- snakemake_wrapper/scripts/metadata.py
+++ snakemake_wrapper/scripts/metadata.py
@@ -1,20 +1,16 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""
-Created on Wed Jul 17 09:35:27 2024
 
-@author: jlehle
-"""
 import os, sys
 
-os.chdir(os.environ['HOME']+'/OmniScrape/tmp')
+os.chdir(output_dir)
 
-#%% Srape out the ftp links from the SRA or GEO search results we will use this later
+#%% Srape out the ftp links from the NCBI search results we will use this later
 import pandas as pd
 pd.options.display.max_colwidth = 10000
-# Reasign X to config once this is working
-X = pd.read_fwf('/master/jlehle/OmniScrape/tmp/kidney_tumor_SRA.txt', header=None)
-X[0]
+# Read in the NCBI search result file
+X = pd.read_fwf(NCBI_search_txt, header=None)
+
 
 ftp_links = X[X[0].str.startswith('FTP download')]
 ftp_links = ftp_links.reset_index(drop=True)
@@ -51,34 +47,19 @@ def url_ok(foo_url):
         return False
 
 
-#%%
-# Multiprocessed code
+#%% Multiprocessed code
 
 from multiprocessing.pool import ThreadPool
 from math import ceil
 from time import perf_counter
-from os import cpu_count
-
 
 ftp_list_input = ftp_list
 
 # Number of items in the list
 N = len(ftp_list_input)
 
-try: 
-    maunal_NCPUS
-    print(f'Creating pool of {manual_NCPUS} threads')
-except NameError:
-    # If no manual number set use half of available CPUs (minimum of 2)
-    if (NCPUS := cpu_count()):
-        NCPUS = max(NCPUS//2, 2)
-        print(f'Creating pool of {NCPUS} threads')
-    else:
-        NCPUS = 2
-        print(f'Creating pool of {NCPUS} threads')
-
-with ThreadPool(NCPUS) as pool:
-    chunksize = ceil(len(ftp_list_input) / NCPUS)
+with ThreadPool(computing_threads) as pool:
+    chunksize = ceil(len(ftp_list_input) / computing_threads)
     start = perf_counter()
     results = list(pool.map_async(url_ok, ftp_list_input, chunksize=chunksize).get())
     end = perf_counter()
@@ -86,7 +67,6 @@ with ThreadPool(NCPUS) as pool:
     print(f'Duration={end-start:.4f}s')
 
 
-#%%
 def countX(lst, x):
     count = 0
     for ele in lst:
@@ -94,41 +74,64 @@ def countX(lst, x):
             count = count + 1
     return count
 
-
-#%%
-print(f'''Of the initiaimport wgetl {len(ftp_links)} datasets provided from the GEO search,
+print(f'''Of the initial imported {len(ftp_links)} datasets provided from the NCBI search,
       {len(ftp_list)} point to GEO FTP pages.
       Of these {len(ftp_list)} FTP pages {countX(results, True)} returned 200 GET statuses for having GEO *.soft files.''')
 
-#%%
+
 from itertools import compress
 ftp_list = list(compress(ftp_list, results))
 len(ftp_list)
-#%% Go fetch all the valid GEO ftp links and get the .soft files
+
+# Go fetch all the valid GEO ftp links and get the .soft files
+# These will be needed to fetch all the GEO ftp files 
 import wget
 import GEOparse
+
+# These are needed to get the GSE files to SRR file names
+import subprocess
+subprocess.run(["pysradb", "--version"])
+
+from pysradb.search import GeoSearch
 # Make a big dictionary that contains all the .soft file information
 gse_dict = {}
 for url in ftp_list:
-    wget_this = url + 'soft/' + url[:-1].split('/', -1)[-1] + '_family.soft.gz'
-    filename = wget.download(wget_this)
-    gse = GEOparse.get_GEO(filepath=filename)
-    gse_dict[url[:-1].split('/', -1)[-1]] = gse.phenotype_data
+    try:
+        wget_this = url + 'soft/' + url[:-1].split('/', -1)[-1] + '_family.soft.gz'
+        filename = wget.download(wget_this)
+        gse = GEOparse.get_GEO(filepath=filename)
+        gse_dict[url[:-1].split('/', -1)[-1]] = gse.phenotype_data
+        instance = GeoSearch(2, 200, geo_query=url[:-1].split('/', -1)[-1])
+        instance.search()
+        df = instance.get_df()
+        data_pysrad = {'SRR': df.run_1_accession.tolist(), 
+                'geo_accession': df.sample_alias.tolist(),
+                'Rep': list(range(1, len(df.run_1_accession)+1))}
+        df_pysrad = pd.DataFrame(data_pysrad)
+        gse_dict[url[:-1].split('/', -1)[-1]] = gse_dict[url[:-1].split('/', -1)[-1]].set_index('geo_accession').merge(df_pysrad, right_on='geo_accession', left_index=True)
+    except:
+        pass
 
-
-#%% Call out the number of bio samples that are available from the .soft files before filtering
+# Call out the number of bio samples that are available from the .soft files before filtering
 num_rows = []
 unique_columns = []
 for key in gse_dict.keys():
     num_rows.append(len(gse_dict[key]))
     unique_columns = [*unique_columns, *gse_dict[key].columns]
 
-#%%
 print(f'''From the {countX(results, True)} FTP pages that returned 200 GET 
       statuses for having GEO *.soft files. 
       There are an available {sum(num_rows)} samples that are available for download.''')
-#%%
-import yaml
 
-with open('dictionary_file.yml', 'w') as yaml_file:
-     yaml.dump(gse_dict, stream=yaml_file, default_flow_style=False)
+# Save the file This would be the stopping point for the first snakemake rule
+import pickle
+
+with open('dictionary_file.pkl', 'wb') as pkl_file:
+    pickle.dump(gse_dict, pkl_file)
+    print('Dictionary saved successfully.')
+    
+
+# End file
+import sys
+
+sys.exit()
